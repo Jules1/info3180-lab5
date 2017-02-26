06863f95ad8db7d34d0affeb009186b1fb8802ac
Jules <julesingledew@gmail.com>
2017-02-26 17:46:28 +0000

    step 10 - logout created

--- app/views.py
+++ app/views.py
@@ -57,7 +57,7 @@ def login():
 def load_user(id):
     return UserProfile.query.get(int(id))
 
-@app.route('/secure-page/')
+@app.route('/secure-page')
 @login_required
 def secure_page():
     return render_template('secure_page.html')
